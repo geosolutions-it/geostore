24bf3ad0f421ef2e75f22192b3977efe69db668b
ETj <etj@geo-solutions.it>
2012-05-28 19:19:07 +0200

    Porting fixes in RESTExtJsServiceImpl from old repo.

--- src/server/modules/rest/extjs/src/main/java/it/geosolutions/geostore/services/rest/impl/RESTExtJsServiceImpl.java
+++ src/server/modules/rest/extjs/src/main/java/it/geosolutions/geostore/services/rest/impl/RESTExtJsServiceImpl.java
@@ -1,11 +1,6 @@
-/*
- * $ Header: it.geosolutions.georepo.services..rest.impl.RESTExtJsServiceImpl ,v. 0.1 9-set-2011 10.39.58 created by tobaro <tobia.dipisa at geo-solutions.it> $
- * $ Revision: 0.1 $
- * $ Date: 8-set-2011 10.39.58 $
+/* ====================================================================
  *
- * ====================================================================
- *
- * Copyright (C) 2007 - 2011 GeoSolutions S.A.S.
+ * Copyright (C) 2007 - 2012 GeoSolutions S.A.S.
  * http://www.geo-solutions.it
  *
  * GPLv3 + Classpath exception
@@ -42,6 +37,7 @@ import it.geosolutions.geostore.services.rest.exception.InternalErrorWebEx;
 import it.geosolutions.geostore.services.rest.utils.GeoStorePrincipal;
 
 import java.security.Principal;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -54,142 +50,169 @@ import net.sf.json.JSONObject;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-/** 
+/**
  * Class RESTExtJsServiceImpl.
  * 
  * @author Tobia di Pisa (tobia.dipisa at geo-solutions.it)
- *
+ * 
  */
-public class RESTExtJsServiceImpl implements RESTExtJsService{
+public class RESTExtJsServiceImpl implements RESTExtJsService {
 
-	private final static Logger LOGGER = Logger.getLogger(RESTExtJsServiceImpl.class);
-	
-    private ResourceService resourceService;
+	private final static Logger LOGGER = Logger
+			.getLogger(RESTExtJsServiceImpl.class);
 
-    /**
-     * @param resourceService
-     */
-    public void setResourceService(ResourceService resourceService) {
-        this.resourceService = resourceService;
-    }
+	private ResourceService resourceService;
 
-	/* (non-Javadoc)
-	 * @see it.geosolutions.geostore.services.rest.RESTExtJsService#getAllResources(javax.ws.rs.core.UriInfo, javax.ws.rs.core.SecurityContext, java.lang.String, java.lang.Integer, java.lang.Integer)
+	/**
+	 * @param resourceService
+	 */
+	public void setResourceService(ResourceService resourceService) {
+		this.resourceService = resourceService;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * it.geosolutions.geostore.services.rest.RESTExtJsService#getAllResources
+	 * (javax.ws.rs.core.UriInfo, javax.ws.rs.core.SecurityContext,
+	 * java.lang.String, java.lang.Integer, java.lang.Integer)
 	 */
 	@Override
-	public String getAllResources(SecurityContext sc, String nameLike, 
+	public String getAllResources(SecurityContext sc, String nameLike,
 			Integer start, Integer limit) throws BadRequestWebEx {
-		
-		if(start == null || limit == null)
+
+		if (start == null || limit == null)
 			throw new BadRequestWebEx("Request parameters are missing !");
-		
-		if(LOGGER.isInfoEnabled())
+
+		if (LOGGER.isInfoEnabled())
 			LOGGER.info("Retrieving the paginated resource list ... ");
-	
-		User authUser = extractAuthUser(sc); 
+
+		User authUser = extractAuthUser(sc);
 
 		int page = start == 0 ? start : start / limit;
 
 		try {
-			List<ShortResource> resources = resourceService.getList(nameLike, page, limit, authUser);
-			
+			nameLike = nameLike.replaceAll("[*]", "%");
+			List<ShortResource> resources = resourceService.getList(nameLike,
+					page, limit, authUser);
+
 			long count = 0;
-			if(resources != null && resources.size() > 0)
+			if (resources != null && resources.size() > 0)
 				count = resourceService.getCount(nameLike);
-			
-			JSONObject result = makeJSONResult(true, count, resources);				
+
+			JSONObject result = makeJSONResult(true, count, resources);
 			return result.toString();
-			
+
 		} catch (BadRequestServiceEx e) {
-			if(LOGGER.isEnabledFor(Level.ERROR))
+			if (LOGGER.isEnabledFor(Level.ERROR))
 				LOGGER.error(e.getMessage());
 
-			JSONObject obj = makeJSONResult(false, 0, null);	
+			JSONObject obj = makeJSONResult(false, 0, null);
 			return obj.toString();
 		}
 	}
-	
+
 	/**
 	 * @param success
 	 * @param count
 	 * @param resources
 	 * @return JSONObject
 	 */
-	private JSONObject makeJSONResult(boolean success, long count, List<ShortResource> resources){
+	private JSONObject makeJSONResult(boolean success, long count,
+			List<ShortResource> resources) {
 		JSONObject jsonObj = new JSONObject();
 		jsonObj.put("success", success);
 		jsonObj.put("totalCount", count);
-		
-		if(resources != null){
+
+		if (resources != null) {
 			Iterator<ShortResource> iterator = resources.iterator();
-			
+
 			JSON result;
-			
+
 			int size = resources.size();
-			if(size == 0)
+			if (size == 0)
 				result = null;
 			else if (size > 1)
 				result = new JSONArray();
 			else
 				result = new JSONObject();
-			
-			while(iterator.hasNext()){
+
+			while (iterator.hasNext()) {
 				ShortResource sr = iterator.next();
-				
-				if(sr != null){
+
+				if (sr != null) {
 					JSONObject jobj = new JSONObject();
 					jobj.element("canDelete", sr.isCanDelete());
 					jobj.element("canEdit", sr.isCanEdit());
-					jobj.element("creation", sr.getCreation());
-					jobj.element("description", sr.getDescription());
+					
+					Date date = sr.getCreation();
+					if(date != null)
+						jobj.element("creation", date.toString());
+				
+					date = sr.getLastUpdate();
+					if(date != null)
+						jobj.element("lastUpdate", date.toString());
+					
+					String description = sr.getDescription();
+					if(description != null)
+						jobj.element("description", description);
+					
 					jobj.element("id", sr.getId());
 					jobj.element("name", sr.getName());
-	
-					if(iterator.hasNext())
-						((JSONArray)result).add(jobj);
-					else
-						result = jobj;					
+					
+					String owner = sr.getOwner();
+					if(owner != null)
+						jobj.element("owner", owner);
+
+					if (result instanceof JSONArray)
+						((JSONArray) result).add(jobj);
+					else 
+						result = jobj;
 				}
 			}
-			
-			jsonObj.put("results", result != null ?  result.toString() : "");
-		}else{
+
+			jsonObj.put("results", result != null ? result.toString() : "");
+		} else {
 			jsonObj.put("results", "");
 		}
-		
+
 		return jsonObj;
 	}
-	
-    /**
-     * @return User - The authenticated user that is accessing this service, or null if guest access.
-     */
-    private User extractAuthUser(SecurityContext sc) throws InternalErrorWebEx {
-        if(sc == null)
-            throw new InternalErrorWebEx("Missing auth info");
-        else {
-            Principal principal = sc.getUserPrincipal();
-            if(principal == null){
-    			if(LOGGER.isInfoEnabled())
-    				LOGGER.info("Missing auth principal");
-    			throw new InternalErrorWebEx("Missing auth principal");
-            }
-                
-            if( ! (principal instanceof GeoStorePrincipal )){
-    			if(LOGGER.isInfoEnabled())
-    				LOGGER.info("Missing auth principal");
-    			throw new InternalErrorWebEx("Mismatching auth principal (" + principal.getClass() + ")");
-            }
 
-            GeoStorePrincipal gsp = (GeoStorePrincipal)principal;
-            
-            //
-            // may be null if guest
-            //
-            User user = gsp.getUser(); 
+	/**
+	 * @return User - The authenticated user that is accessing this service, or
+	 *         null if guest access.
+	 */
+	private User extractAuthUser(SecurityContext sc) throws InternalErrorWebEx {
+		if (sc == null)
+			throw new InternalErrorWebEx("Missing auth info");
+		else {
+			Principal principal = sc.getUserPrincipal();
+			if (principal == null) {
+				if (LOGGER.isInfoEnabled())
+					LOGGER.info("Missing auth principal");
+				throw new InternalErrorWebEx("Missing auth principal");
+			}
 
-            LOGGER.info("Accessing service with user " + (user == null ? "GUEST" : user.getName()));
-            return user;
-        }
-    }
+			if (!(principal instanceof GeoStorePrincipal)) {
+				if (LOGGER.isInfoEnabled())
+					LOGGER.info("Missing auth principal");
+				throw new InternalErrorWebEx("Mismatching auth principal ("
+						+ principal.getClass() + ")");
+			}
+
+			GeoStorePrincipal gsp = (GeoStorePrincipal) principal;
+
+			//
+			// may be null if guest
+			//
+			User user = gsp.getUser();
+
+			LOGGER.info("Accessing service with user "
+					+ (user == null ? "GUEST" : user.getName()));
+			return user;
+		}
+	}
 
 }
