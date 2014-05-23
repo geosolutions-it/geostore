54af9b8fa17219835ca9328c023eff34ccb263a0
tobia <tobia.dipisa@geo-solutions.it>
2014-05-23 16:02:27 +0200

    Fixed migration file and added notes about PW recoding

--- doc/sql/migration/h2/h2-migration-from-v1.1.1-to-1.2.sql
+++ doc/sql/migration/h2/h2-migration-from-v1.1.1-to-1.2.sql
@@ -19,5 +19,13 @@ create table gs_usergroup_members (user_id int8 not null, group_id int8 not null
 alter table gs_usergroup_members add constraint FKFDE460DB62224F72 foreign key (user_id) references gs_user;
 alter table gs_usergroup_members add constraint FKFDE460DB9EC981B7 foreign key (group_id) references gs_usergroup;
 
-INSERT INTO gs_usergroup(groupname, description, enabled) VALUES ('testGroup1', 'description', 'Y');
+INSERT INTO gs_usergroup(groupname, description, enabled) VALUES ('everyone', 'description', 'Y');
 INSERT INTO gs_user(name, user_role, enabled) VALUES ('guest', 'GUEST', 'Y');
+
+INSERT INTO gs_security(id,canread,canwrite,group_id,resource_id)
+SELECT true,false,(SELECT id FROM gs_usergroup WHERE groupname='everyone'),gs_resource.id
+FROM gs_resource INNER JOIN gs_category ON gs_resource.category_id=gs_category.id
+WHERE gs_category.name='MAP';
+
+INSERT INTO gs_usergroup_members(user_id, group_id) SELECT gsuser.id, gsgroup.id FROM gs_user AS gsuser, gs_usergroup AS gsgroup 
+WHERE gsuser.user_role='GUEST' AND gsgroup.groupname='everyone';
