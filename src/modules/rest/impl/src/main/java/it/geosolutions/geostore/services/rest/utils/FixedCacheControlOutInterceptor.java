170f58005a0146504aa36afc300c0cb37bddc922
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2018-03-08 15:50:14 +0100

    Fix #171. Add fixed CacheControl interceptor

---
+++ src/modules/rest/impl/src/main/java/it/geosolutions/geostore/services/rest/utils/FixedCacheControlOutInterceptor.java
@@ -0,0 +1,41 @@
+package it.geosolutions.geostore.services.rest.utils;
+import java.io.OutputStream;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.apache.cxf.jaxrs.impl.MetadataMap;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+/**
+ * Attach to the response the header CacheControl
+ * 
+ * @author Lorenzo Natali, GeoSolutions SAS
+ *
+ */
+public class FixedCacheControlOutInterceptor extends AbstractPhaseInterceptor<Message> {
+	String value = "no-cache";
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+    public FixedCacheControlOutInterceptor () {
+        super(Phase.MARSHAL);
+    }
+
+    @SuppressWarnings("unchecked")
+	public final void handleMessage(Message message) {
+        OutputStream os = message.getContent(OutputStream.class);
+        if (os == null) {
+            return;
+        }
+        MultivaluedMap<String, Object> headers = (MetadataMap<String, Object>) message.get(Message.PROTOCOL_HEADERS);
+        if (headers == null) {
+            headers = new MetadataMap<String, Object>();
+        }   
+        headers.add("Cache-Control", new String("no-cache"));
+        message.put(Message.PROTOCOL_HEADERS, headers);
+    }
+}
