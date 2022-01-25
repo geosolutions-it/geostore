5fed9ed5862159506b7b2f0d9a774e8ea30c2ed9
Shadab <shadabgada234@gmail.com>
2022-01-25 10:54:46 +0530

    upgraded httpclient

--- src/modules/rest/impl/src/main/java/it/geosolutions/geostore/services/rest/security/WebServiceTokenAuthenticationFilter.java
+++ src/modules/rest/impl/src/main/java/it/geosolutions/geostore/services/rest/security/WebServiceTokenAuthenticationFilter.java
@@ -31,39 +31,42 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.params.HttpClientParams;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.springframework.security.core.Authentication;
 
 /**
  * Token based authentication filter that looks for the token calling an external webservice.
- * 
+ *
  * The url of the service needs to be configured. A placeholder in the url will be replaced by
  * the actual token.
- * 
+ *
  * The result of the web service call will be parsed using given regular expression to:
  *  - check if the token is valid
  *  - extract the user name from the result
- * 
+ *
  * @author Mauro Bartolomeoli
  *
  */
 public class WebServiceTokenAuthenticationFilter extends TokenAuthenticationFilter {
-    
+
     private final static Logger LOGGER = Logger.getLogger(WebServiceTokenAuthenticationFilter.class);
-    
+
     private String url;
-    
+
     // compiled user search regex
     Pattern searchUserRegex = null;
 
     // connection timeout to the mapper web service (in seconds)
-    long connectTimeout = 5;
+    int connectTimeout = 5;
 
     // read timeout to the mapper web service (in seconds)
     int readTimeout = 10;
@@ -71,17 +74,21 @@ public class WebServiceTokenAuthenticationFilter extends TokenAuthenticationFilt
     // optional external httpClient for web service connection (used mainly for tests)
     private HttpClient httpClient = null;
 
+    private RequestConfig connectionConfig;
+
+    HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+
     public WebServiceTokenAuthenticationFilter(String url) {
         super();
         this.url = url;
     }
-    
+
     /**
      * Regular expression to extract the username from the
      * webservice response.
-     * 
+     *
      * The first group in the expression will be used for extraction.
-     * 
+     *
      * @param searchUser
      */
     public void setSearchUser(String searchUser) {
@@ -90,7 +97,7 @@ public class WebServiceTokenAuthenticationFilter extends TokenAuthenticationFilt
 
 
 
-    public void setConnectTimeout(long connectTimeout) {
+    public void setConnectTimeout(int connectTimeout) {
         this.connectTimeout = connectTimeout;
     }
 
@@ -102,36 +109,40 @@ public class WebServiceTokenAuthenticationFilter extends TokenAuthenticationFilt
 
     /**
      * Configures the HTTPClient implementation to be used to connect to the web service.
-     * 
+     *
      * @param httpClient
      */
     public void setHttpClient(HttpClient httpClient) {
         this.httpClient = httpClient;
     }
-    
+
     private HttpClient getHttpClient() {
         if (httpClient == null) {
-            httpClient = new HttpClient();
+            httpClient = clientBuilder.useSystemProperties().build();
         }
         return httpClient;
     }
-    
+
     @Override
     protected Authentication checkToken(String token) {
         String webServiceUrl = url.replace("{token}", token);
         HttpClient client = getHttpClient();
-        client.getParams().setParameter(HttpClientParams.CONNECTION_MANAGER_TIMEOUT, connectTimeout * 1000l);
-        client.getParams().setParameter(HttpClientParams.SO_TIMEOUT, readTimeout * 1000);
-        HttpMethod method = null;
+        connectionConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(connectTimeout * 1000)
+                .setSocketTimeout(readTimeout * 1000)
+                .build();
+
+        HttpRequestBase method = null;
         try {
             LOGGER.debug("Issuing request to webservice: " + url);
-            method = new GetMethod(webServiceUrl);
-            int statusCode = client.executeMethod(method);
-            
-            if (statusCode == HttpStatus.SC_OK) {
+            method = new HttpGet(webServiceUrl);
+            method.setConfig(connectionConfig);
+            HttpResponse httpResponse = client.execute(method);
+
+            if (getStatusCode(httpResponse) == HttpStatus.SC_OK) {
                 // get response content as a single string, without new lines
                 // so that is simpler to apply an extraction regular expression
-                String response = method.getResponseBodyAsString()
+                String response = EntityUtils.toString(httpResponse.getEntity(), "UTF-8")
                         .replace("\r", "").replace("\n", "");
                 if(response != null) {
                     if (searchUserRegex == null) {
@@ -148,8 +159,6 @@ public class WebServiceTokenAuthenticationFilter extends TokenAuthenticationFilt
                     LOGGER.error("No response received from webservice: " + url);
                 }
             }
-        } catch (HttpException e) {
-            LOGGER.error("Error contacting webservice: " + url, e);
         } catch (IOException e) {
             LOGGER.error("Error reading data from webservice: " + url, e);
         } finally {
@@ -158,6 +167,15 @@ public class WebServiceTokenAuthenticationFilter extends TokenAuthenticationFilt
             }
         }
         return null;
-        
-    }    
+
+    }
+
+    public int getStatusCode(HttpResponse response) {
+        if (response != null) {
+            StatusLine statusLine = response.getStatusLine();
+            return statusLine.getStatusCode();
+        } else {
+            return -1;
+        }
+    }
 }
