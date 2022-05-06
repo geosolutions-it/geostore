fd7ebab7f293fe63aef55565969187a02a56d799
taba90 <46672505+taba90@users.noreply.github.com>
2022-05-06 17:10:23 +0200

    Tests for LDAP groups and roles concepts are mixed (#272)

--- src/modules/rest/impl/src/test/java/it/geosolutions/geostore/rest/security/GeoStoreLdapAuthoritiesPopulatorTest.java
+++ src/modules/rest/impl/src/test/java/it/geosolutions/geostore/rest/security/GeoStoreLdapAuthoritiesPopulatorTest.java
@@ -1,11 +1,16 @@
 package it.geosolutions.geostore.rest.security;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.naming.Name;
 import javax.naming.NamingEnumeration;
@@ -16,6 +21,7 @@ import javax.naming.directory.DirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 
+import it.geosolutions.geostore.core.security.SimpleGrantedAuthoritiesMapper;
 import org.junit.Test;
 import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.security.core.GrantedAuthority;
@@ -48,7 +54,24 @@ public class GeoStoreLdapAuthoritiesPopulatorTest {
                         }
                         
                     }, new BasicAttributes());
-                    return new IterableNamingEnumeration(Collections.singletonList(sr));
+
+                    SearchResult sr2 = new SearchResult("cn=group2", null, new MockDirContextOperations() {
+
+                        @Override
+                        public String getNameInNamespace() {
+                            return "cn=group2,ou=groups";
+                        }
+
+                        @Override
+                        public String getStringAttribute(String name) {
+                            if ("cn".equals(name)) {
+                                return "group2";
+                            }
+                            return "";
+                        }
+
+                    }, new BasicAttributes());
+                    return new IterableNamingEnumeration(Arrays.asList(sr, sr2));
                 } else if ("(member=cn=group1,ou=groups)".equals(filter)) {
                     List<SearchResult> groups = new ArrayList<SearchResult>();
                     SearchResult sr = new SearchResult("cn=parentgroup1", null, new MockDirContextOperations() {
@@ -81,7 +104,7 @@ public class GeoStoreLdapAuthoritiesPopulatorTest {
                 new GeoStoreLdapAuthoritiesPopulator(new MockContextSource(ctx), "ou=groups", "ou=roles");
         authoritiesPopulator.setEnableHierarchicalGroups(true);
         Set<GrantedAuthority> authorities = authoritiesPopulator.getGroupMembershipRoles("uid=bill,ou=people", "bill");
-        assertEquals(2, authorities.size());
+        assertEquals(3, authorities.size());
     }
     
     @Test
@@ -91,6 +114,41 @@ public class GeoStoreLdapAuthoritiesPopulatorTest {
                 new GeoStoreLdapAuthoritiesPopulator(new MockContextSource(ctx), "ou=groups", "ou=roles");
         authoritiesPopulator.setEnableHierarchicalGroups(false);
         Set<GrantedAuthority> authorities = authoritiesPopulator.getGroupMembershipRoles("uid=bill,ou=people", "bill");
+        assertEquals(2, authorities.size());
+    }
+
+    @Test
+    public void testDropUnmappedRoles() {
+
+        GeoStoreLdapAuthoritiesPopulator authoritiesPopulator =
+                new GeoStoreLdapAuthoritiesPopulator(new MockContextSource(ctx), "ou=groups", "ou=groups");
+        authoritiesPopulator.setEnableHierarchicalGroups(false);
+        Map<String,String>  mappings=new HashMap<>();
+        mappings.put("ROLE_GROUP2","ROLE_ADMIN");
+        SimpleGrantedAuthoritiesMapper mapper=new SimpleGrantedAuthoritiesMapper(mappings);
+        mapper.setDropUnmapped(true);
+        authoritiesPopulator.setRoleMapper(mapper);
+        Set<GrantedAuthority> authorities = authoritiesPopulator.getGroupMembershipRoles("uid=bill,ou=people", "bill");
+        List<String> expected=Arrays.asList("ROLE_ADMIN","GROUP1","GROUP2");
+        List<String> stringAuthorities=authorities.stream().map(a->a.getAuthority()).collect(Collectors.toList());
+        assertEquals(expected.size(), stringAuthorities.size());
+        assertTrue(stringAuthorities.containsAll(expected));
+    }
+
+
+    @Test
+    public void testDropUnmappedGroups() {
+
+        GeoStoreLdapAuthoritiesPopulator authoritiesPopulator =
+                new GeoStoreLdapAuthoritiesPopulator(new MockContextSource(ctx), "ou=groups", "ou=roles");
+        authoritiesPopulator.setEnableHierarchicalGroups(false);
+        Map<String,String>  mappings=new HashMap<>();
+        mappings.put("GROUP2","MAPPED_GROUP");
+        SimpleGrantedAuthoritiesMapper mapper=new SimpleGrantedAuthoritiesMapper(mappings);
+        mapper.setDropUnmapped(true);
+        authoritiesPopulator.setGroupMapper(mapper);
+        Set<GrantedAuthority> authorities = authoritiesPopulator.getGroupMembershipRoles("uid=bill,ou=people", "bill");
         assertEquals(1, authorities.size());
+        assertEquals("MAPPED_GROUP",authorities.iterator().next().getAuthority());
     }
 }
