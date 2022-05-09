0b546ab59ab9168c5a1f13ae49bef242e370974c
taba90 <46672505+taba90@users.noreply.github.com>
2022-05-09 08:09:10 +0200

    Backport ldap fix to 1.8.x (#274)
    * Merge pull request #266 from geosolutions-it/265_17x_ldap_roles
    
    * Tests for LDAP groups and roles concepts are mixed (#272)
    
    Co-authored-by: Nuno Oliveira <nuno.oliveira@geo-solutions.it>

--- src/core/security/src/test/java/it/geosolutions/geostore/core/security/SimpleGrantedAuthoritiesMapperTest.java
+++ src/core/security/src/test/java/it/geosolutions/geostore/core/security/SimpleGrantedAuthoritiesMapperTest.java
@@ -35,4 +35,15 @@ public class SimpleGrantedAuthoritiesMapperTest {
        assertEquals("B", mapped.iterator().next().getAuthority());
    }
 
+   @Test
+   public void testDropUnmappedAuthiorities() {
+        mapper.setDropUnmapped(true);
+        roleMappings.put("A", "B");
+        authorities.add(new SimpleGrantedAuthority("A"));
+        authorities.add(new SimpleGrantedAuthority("C"));
+        Collection<? extends GrantedAuthority> mapped = mapper.mapAuthorities(authorities);
+        assertEquals(1, mapped.size());
+        assertEquals("B", mapped.iterator().next().getAuthority());
+   }
+
 }
