458465fdaf549446baeb1e7d22b0a61a3be165cf
Alessandro Ricchiuti <alessandro.ricchiuti@geosolutionsgroup.com>
2025-01-29 16:53:24 +0100

    Added DB migration scripts.

--- doc/sql/migration/oracle/oracle-migration-from-v.2.1.0-to-v2.3.0.sql
+++ doc/sql/migration/oracle/oracle-migration-from-v.2.1.0-to-v2.3.0.sql
@@ -1,3 +1,4 @@
+-- Tags
 create table gs_tag (
     id number(19,0) not null,
     color varchar2(255 char) not null,
@@ -12,7 +13,6 @@ create table gs_resource_tags (
     primary key (tag_id, resource_id)
 );
 
--- Add foreign key constraints to gs_resource_tags
 alter table gs_resource_tags
     add constraint fk_resource_tags_resource
     foreign key (resource_id)
@@ -23,3 +23,21 @@ alter table gs_resource_tags
     add constraint fk_resource_tags_tag
     foreign key (tag_id)
     references gs_tag(id);
+
+-- Favorites
+create table gs_user_favorites (
+    user_id number(19,0) not null,
+    resource_id number(19,0) not null,
+    primary key (user_id, resource_id)
+);
+
+alter table gs_user_favorites
+    add constraint fk_user_favorites_resource
+    foreign key (resource_id)
+    references gs_resource(id)
+    on delete cascade;
+
+alter table gs_user_favorites
+    add constraint fk_user_favorites_user
+    foreign key (user_id)
+    references gs_user(id);
