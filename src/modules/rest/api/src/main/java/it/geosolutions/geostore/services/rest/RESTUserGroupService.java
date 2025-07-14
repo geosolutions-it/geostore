a47d50a820a04e70281a802589bd472ae2fa9281
afabiani <alessio.fabiani@geosolutionsgroup.com>
2025-07-14 14:38:23 +0200

    Code format

--- src/modules/rest/api/src/main/java/it/geosolutions/geostore/services/rest/RESTUserGroupService.java
+++ src/modules/rest/api/src/main/java/it/geosolutions/geostore/services/rest/RESTUserGroupService.java
@@ -24,24 +24,18 @@ import it.geosolutions.geostore.services.rest.exception.NotFoundWebEx;
 import it.geosolutions.geostore.services.rest.model.RESTUserGroup;
 import it.geosolutions.geostore.services.rest.model.ShortResourceList;
 import it.geosolutions.geostore.services.rest.model.UserGroupList;
-import java.util.List;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.SecurityContext;
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 import org.springframework.security.access.annotation.Secured;
 
-/** @author DamianoG */
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.util.List;
+
+/**
+ * @author DamianoG
+ */
 public interface RESTUserGroupService {
 
     @POST
@@ -98,15 +92,15 @@ public interface RESTUserGroupService {
     /**
      * Returns groups that match searching criteria with pagination.
      *
-     * @param sc the security context
-     * @param page the requested page number
-     * @param entries max entries for page
-     * @param nameLike a sub-string to search in group name with ILIKE operator
-     * @param all if <code>true</code> adds to result the 'everyone' group if it matches the
-     *     searching criteria
+     * @param sc           the security context
+     * @param page         the requested page number
+     * @param entries      max entries for page
+     * @param nameLike     a sub-string to search in group name with ILIKE operator
+     * @param all          if <code>true</code> adds to result the 'everyone' group if it matches the
+     *                     searching criteria
      * @param includeUsers if to include group users in the results
      * @return a list of groups that match searching criteria with pagination.
-     * @throws BadRequestWebEx
+     * @throws BadRequestWebEx Exception
      */
     @GET
     @Path("/")
