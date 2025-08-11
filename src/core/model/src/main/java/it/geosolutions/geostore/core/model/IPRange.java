44c3fa1dd00e6f11bd5436780fa4e8023a18fb94
Alessandro Ricchiuti <alessandro.ricchiuti@geosolutionsgroup.com>
2025-08-11 11:18:01 +0200

    Aligned IPRange class to the other project models .

--- src/core/model/src/main/java/it/geosolutions/geostore/core/model/IPRange.java
+++ src/core/model/src/main/java/it/geosolutions/geostore/core/model/IPRange.java
@@ -1,8 +1,39 @@
+/* ====================================================================
+ *
+ * Copyright (C) 2025 GeoSolutions S.A.S.
+ * http://www.geo-solutions.it
+ *
+ * GPLv3 + Classpath exception
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ *
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by developers
+ * of GeoSolutions.  For more information on GeoSolutions, please see
+ * <http://www.geo-solutions.it/>.
+ *
+ */
 package it.geosolutions.geostore.core.model;
 
 import java.io.Serializable;
-import java.util.Objects;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity(name = "IPRange")
@@ -44,30 +75,74 @@ public class IPRange implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof IPRange)) return false;
-        IPRange ipRange = (IPRange) o;
-        return Objects.equals(getId(), ipRange.getId())
-                && Objects.equals(getCidr(), ipRange.getCidr())
-                && Objects.equals(getDescription(), ipRange.getDescription());
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = (prime * result) + ((id == null) ? 0 : id.hashCode());
+        result = (prime * result) + ((cidr == null) ? 0 : cidr.hashCode());
+        result = (prime * result) + ((description == null) ? 0 : description.hashCode());
+
+        return result;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getCidr(), getDescription());
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        IPRange other = (IPRange) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        if (cidr == null) {
+            if (other.cidr != null) {
+                return false;
+            }
+        } else if (!cidr.equals(other.cidr)) {
+            return false;
+        }
+        if (description == null) {
+            if (other.description != null) {
+                return false;
+            }
+        } else if (!description.equals(other.description)) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public String toString() {
-        return "IPRange{"
-                + "id="
-                + id
-                + ", cidr='"
-                + cidr
-                + '\''
-                + ", description='"
-                + description
-                + '\''
-                + '}';
+        StringBuilder builder = new StringBuilder();
+        builder.append(getClass().getSimpleName()).append('[');
+
+        if (id != null) {
+            builder.append("id=").append(id);
+        }
+
+        if (cidr != null) {
+            builder.append(", ");
+            builder.append("cidr=").append(cidr);
+        }
+
+        if (description != null) {
+            builder.append(", ");
+            builder.append("description=").append(description);
+        }
+
+        builder.append(']');
+
+        return builder.toString();
     }
 }
