dd5e1f115f989b13fae47db2f89d148876926820
Alessandro Ricchiuti <alessandro.ricchiuti@geosolutionsgroup.com>
2026-06-18 13:33:02 +0200

    Set HeaderWriterFilter's shouldWriteHeadersEagerly to true.

---
+++ src/modules/rest/impl/src/main/java/it/geosolutions/geostore/services/rest/security/HeaderWriterFilterEagerConfig.java
@@ -0,0 +1,48 @@
+/* ====================================================================
+ *
+ * Copyright (C) 2026 GeoSolutions S.r.l.
+ * http://www.geo-solutions.it
+ *
+ * GPLv3 + Classpath exception
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.
+ *
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by developers
+ * of GeoSolutions.  For more information on GeoSolutions, please see
+ * <http://www.geo-solutions.it/>.
+ *
+ */
+package it.geosolutions.geostore.services.rest.security;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.security.web.header.HeaderWriterFilter;
+
+/**
+ * Ensures HTTP security headers are written before the response is
+ * committed by enabling eager header writing on the {@link HeaderWriterFilter} created by Spring
+ * Security.
+ */
+public class HeaderWriterFilterEagerConfig implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof HeaderWriterFilter) {
+            ((HeaderWriterFilter) bean).setShouldWriteHeadersEagerly(true);
+        }
+        return bean;
+    }
+}
